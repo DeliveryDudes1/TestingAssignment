4d9cab17d5b4a34e6596039b0c50b4d48cc90a84
ArifMatin <arif.matin@mail.itsligo.ie>
2019-03-11 17:03:02 +0000

    first

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <asp:Label ID="Label1" runat="server" Text="Age"></asp:Label>
+
+    <asp:TextBox ID="Age" runat="server"></asp:TextBox>
+
+    <asp:Label ID="Label2" runat="server" Text="Gender"></asp:Label>
+
+    <asp:TextBox ID="Gender" runat="server"></asp:TextBox>
+
+
+
+</asp:Content>
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class InsuranceService
+    {
+        public float CalcPremium(int age, string gender)
+        {
+            float premium;
+
+            if (gender == "female")
+                if ((age >= 18) && (age <= 30))
+                    premium = 5.0f;
+                else
+                    if (age >= 31)
+                    premium = 2.0f;
+                else
+                    premium = 0.0f;
+            else
+                 if (gender == "male")
+                if ((age >= 18) && (age <= 35))
+                    premium = 6.0f;
+                else
+                    if (age >= 36)
+                    premium = 5.0f;
+                else
+                    premium = 0.0f;
+            else
+                premium = 0.0f;
+
+            if (age >= 50)
+                premium = premium * 0.15f;
+            return premium;
+        }
+    }
+}
