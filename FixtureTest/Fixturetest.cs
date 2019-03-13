ab0f77c7d82822aef18eaf55e8538aba69facd8f
ArifMatin <arif.matin@mail.itsligo.ie>
2019-03-13 22:32:34 +0000

    more test

---
+++ FixtureTest/Fixturetest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1;
+
+namespace FixtureTest
+{
+    public class Fixturetest
+    {
+        public static InsuranceService SUT;
+        public static int age;
+        public static string gender;
+
+        public float Premium()
+        {
+            SUT = new InsuranceService();
+            return SUT.CalcPremium(age,gender);
+        }
+
+    }
+}
---
+++ UnitTest/TestClass.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class TestClass
+    {
+        //3 test for male and 3 for female
+
+        public static InsuranceService SUT;
+
+        [SetUp]
+        public static void Init()
+        {
+            SUT = new InsuranceService();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            float result = SUT.CalcPremium(12, "male");
+
+            Assert.That(result, Is.EqualTo(0f));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            float result = SUT.CalcPremium(12, "female");
+
+            Assert.That(result, Is.EqualTo(0f));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            float result = SUT.CalcPremium(22, "male");
+
+            Assert.That(result, Is.EqualTo(6f));
+
+        }
+
+        [Test]
+        public void Test4()
+        {
+            float result = SUT.CalcPremium(22, "female");
+
+            Assert.That(result, Is.EqualTo(5f));
+
+        }
+    }
+}
